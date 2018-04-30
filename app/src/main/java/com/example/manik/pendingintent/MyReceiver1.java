b51a36e3082122cbb56992f3f3e4804ef5793d74
Manik Anand <infomanik.anand@gmail.com>
2018-04-30 22:05:34 +0530

    Init

---
+++ app/src/main/java/com/example/manik/pendingintent/MyReceiver1.java
@@ -0,0 +1,19 @@
+package com.example.manik.pendingintent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MyReceiver1 extends BroadcastReceiver {
+    public MyReceiver1() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        Toast.makeText(context, "Alarm is Ringing",Toast.LENGTH_SHORT).show();
+
+    }
+}
